98e8c5c8950ca46f7f8d115763bb44ff48693f41
jizots <hotphilly1776@yahoo.co.jp>
2023-09-20 15:56:13 +0900

    fix miss

--- srcs/add_string_to_array.c
+++ srcs/add_string_to_array.c
@@ -6,7 +6,7 @@
 /*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 15:11:40 by sotanaka          #+#    #+#             */
-/*   Updated: 2023/09/20 15:43:14 by sotanaka         ###   ########.fr       */
+/*   Updated: 2023/09/20 15:47:23 by sotanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,7 @@ char	**add_string_to_array(char *src_add, char **old_array)
 	size_alloc = count_row_of_matrix(old_array) + 2;
 	new_array = malloc(sizeof(char *) * size_alloc);
 	if (new_array == NULL)
-	{
-		write(2, "Error. Alloc at create_string_array\n", 36);
 		return (NULL);
-	}
 	ft_memcpy(new_array, old_array, sizeof(char *) * (size_alloc - 1));
 	new_array[size_alloc - 2] = src_add;
 	new_array[size_alloc - 1] = NULL;
