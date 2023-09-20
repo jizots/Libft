2f3e1b403a0659a337a29dc04b60d9bcbfc7f5e9
jizots <hotphilly1776@yahoo.co.jp>
2023-09-20 16:22:57 +0900

    fix miss

--- srcs/is_valid_matrix.c
+++ srcs/is_valid_matrix.c
@@ -6,20 +6,20 @@
 /*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 18:17:43 by hotph             #+#    #+#             */
-/*   Updated: 2023/09/13 18:47:18 by sotanaka         ###   ########.fr       */
+/*   Updated: 2023/09/20 16:21:16 by sotanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libsft_utils.h"
 
-bool	is_valid_matrix(int num_of_row, char *matrix[], void *f(const char *))
+bool	is_valid_matrix(int num_of_row, char *matrix[], bool (*f)(const char *))
 {
 	int	i;
 
 	i = 1;
 	while (i < num_of_row)
 	{
-		if ((f)(matrix[i]) != 0)
+		if ((f)(matrix[i]) == false)
 			return (false);
 		i++;
 	}
